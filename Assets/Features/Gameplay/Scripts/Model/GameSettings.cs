00567659735326cd944876577228fc62c047d40c
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2024-12-03 22:57:12 +0700

    Создан скрипт настроек игры

---
+++ Assets/Features/Gameplay/Scripts/Model/GameSettings.cs
@@ -0,0 +1,28 @@
+﻿namespace TicTacToe3D.Features.Gameplay
+{
+    /// <summary>
+    /// Настройки игры
+    /// </summary>
+    public class GameSettings
+    {
+        /// <summary>
+        /// Режим игры
+        /// </summary>
+        public GameMode GameMode
+        {
+            get => _gameMode;
+            set => _gameMode = value;
+        }
+        private GameMode _gameMode = GameMode.WithFriend;
+
+        /// <summary>
+        /// Цвет шара
+        /// </summary>
+        public BallColor BallColor
+        {
+            get => _ballColor;
+            set => _ballColor = value;
+        }
+        private BallColor _ballColor = BallColor.White;
+    }
+}
