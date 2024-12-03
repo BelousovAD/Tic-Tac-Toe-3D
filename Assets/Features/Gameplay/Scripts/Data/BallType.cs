59f427e2f7f63d71d821b366b7cbdd5321536e10
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2024-12-03 10:00:03 +0700

    Создан скрипт типа шара

---
+++ Assets/Features/Gameplay/Scripts/Data/BallType.cs
@@ -0,0 +1,12 @@
+﻿namespace TicTacToe3D.Features.Gameplay
+{
+    /// <summary>
+    /// Тип шара
+    /// </summary>
+    public enum BallType
+    {
+        None = 0,
+        Black,
+        White,
+    }
+}
