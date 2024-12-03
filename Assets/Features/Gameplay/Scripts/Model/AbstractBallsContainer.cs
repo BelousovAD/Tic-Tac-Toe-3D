2b6d9a6dd206e12e60ce487420f8da902c85e2e7
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2024-12-03 10:02:36 +0700

    Создан скрипт абстрактного контейнера шаров для определения победителя

---
+++ Assets/Features/Gameplay/Scripts/Model/AbstractBallsContainer.cs
@@ -0,0 +1,70 @@
+﻿namespace TicTacToe3D.Features.Gameplay
+{
+    /// <summary>
+    /// Абстрактный контейнер шаров для определения победителя
+    /// </summary>
+    public abstract class AbstractBallsContainer
+    {
+        #region Properties
+
+        /// <summary>
+        /// Заполнен ли контейнер
+        /// </summary>
+        public virtual bool IsFull
+        {
+            get => isFull;
+            protected set
+            {
+                if (value != isFull)
+                {
+                    isFull = value;
+                }
+            }
+        }
+        protected bool isFull = false;
+
+        /// <summary>
+        /// Победитель
+        /// </summary>
+        public virtual BallType Winner
+        {
+            get => winner;
+            protected set
+            {
+                if (value != winner)
+                {
+                    winner = value;
+                }
+            }
+        }
+        protected BallType winner = BallType.None;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Пытается добавить шар в контейнер
+        /// </summary>
+        /// <param name="ball">Добавляемый шар</param>
+        public abstract void TryAddBall(BallModel ball);
+
+        protected virtual void UpdateFilledStatus(AbstractBallsContainer ballsContainer)
+        {
+            if (!ballsContainer.IsFull)
+            {
+                IsFull = false;
+            }
+        }
+
+        protected virtual void UpdateWinner(AbstractBallsContainer ballsContainer)
+        {
+            if (ballsContainer.Winner != BallType.None)
+            {
+                Winner = ballsContainer.Winner;
+            }
+        }
+
+        #endregion
+    }
+}
