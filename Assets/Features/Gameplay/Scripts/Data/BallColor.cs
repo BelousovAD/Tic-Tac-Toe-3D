26df1f4de8c9c23ba436ac2a830a9fb3d12d047e
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2024-12-03 22:56:13 +0700

    Создано перечисление цветов шара

---
+++ Assets/Features/Gameplay/Scripts/Data/BallColor.cs
@@ -0,0 +1,11 @@
+﻿namespace TicTacToe3D.Features.Gameplay
+{
+    /// <summary>
+    /// Цвет шара
+    /// </summary>
+    public enum BallColor
+    {
+        White = 0,
+        Black,
+    }
+}
