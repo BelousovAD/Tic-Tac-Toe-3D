b80838bc31b5e2ebd55ee3c614b8b3055c1e39d5
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2024-12-03 10:01:18 +0700

    Создан скрипт модели шара

---
+++ Assets/Features/Gameplay/Scripts/Model/BallModel.cs
@@ -0,0 +1,28 @@
+﻿namespace TicTacToe3D.Features.Gameplay
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Шар
+    /// </summary>
+    public class BallModel
+    {
+        /// <summary>
+        /// Тип шара
+        /// </summary>
+        public BallType BallType => _ballType;
+        private BallType _ballType = BallType.Black;
+
+        /// <summary>
+        /// Позиция шара в кубе
+        /// </summary>
+        public Vector3Int Position => _position;
+        private Vector3Int _position = Vector3Int.zero;
+
+        public BallModel(BallType ballType, Vector3Int position)
+        {
+            _ballType = ballType;
+            _position = position;
+        }
+    }
+}
