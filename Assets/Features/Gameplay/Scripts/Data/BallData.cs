ae56ed90e683e7b5c5ee87f354420e33f6b78bf5
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2024-12-03 09:59:09 +0700

    Создан скрипт данных шара

---
+++ Assets/Features/Gameplay/Scripts/Data/BallData.cs
@@ -0,0 +1,25 @@
+﻿namespace TicTacToe3D.Features.Gameplay
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Данные шара
+    /// </summary>
+    [CreateAssetMenu(fileName = nameof(BallData), menuName = "TicTacToe3D/Features/Gameplay/New " + nameof(BallData))]
+    public class BallData : ScriptableObject
+    {
+        /// <summary>
+        /// Тип шара
+        /// </summary>
+        public BallType Type => _type;
+        [SerializeField]
+        private BallType _type = BallType.Black;
+
+        /// <summary>
+        /// Префаб шара
+        /// </summary>
+        public GameObject Prefab => _prefab;
+        [SerializeField]
+        private GameObject _prefab = default;
+    }
+}
