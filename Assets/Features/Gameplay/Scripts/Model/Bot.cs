cef4faf4c897b1caa4ea633f0a1c1c9349a1eba7
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2024-12-04 09:20:32 +0700

    Создан скрипт бота

---
+++ Assets/Features/Gameplay/Scripts/Model/Bot.cs
@@ -0,0 +1,33 @@
+﻿namespace TicTacToe3D.Features.Gameplay
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Бот
+    /// </summary>
+    public class Bot : Player
+    {
+        #region Properties
+
+        private BallSpawnPosition[,] m_ballSpawnPositions;
+
+        #endregion
+
+        #region Methods
+
+        public Bot(BallSpawnPosition[] ballSpawnPositions)
+            : base("Bot")
+        {
+            int dimension = (int)Mathf.Sqrt(ballSpawnPositions.Length);
+            m_ballSpawnPositions = new BallSpawnPosition[dimension, dimension];
+
+            foreach (BallSpawnPosition ballSpawnPosition in ballSpawnPositions)
+            {
+                Vector3Int position = ballSpawnPosition.BallPosition;
+                m_ballSpawnPositions[position.x, position.y] = ballSpawnPosition;
+            }
+        }
+
+        #endregion
+    }
+}
