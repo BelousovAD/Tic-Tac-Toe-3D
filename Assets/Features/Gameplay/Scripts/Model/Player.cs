e08d55e02d6a5f8bf521ff8d6cea486483262e09
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2024-12-04 09:20:04 +0700

    Создан скрипт игрока

---
+++ Assets/Features/Gameplay/Scripts/Model/Player.cs
@@ -0,0 +1,37 @@
+﻿namespace TicTacToe3D.Features.Gameplay
+{
+    /// <summary>
+    /// Игрок
+    /// </summary>
+    public class Player
+    {
+        #region Properties
+
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public int Id => _id;
+        private int _id = 0;
+
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Name => _name;
+        private string _name;
+        private static int _idCount = 0;
+
+        #endregion
+
+        #region Methods
+
+        public Player()
+        {
+            _id = _idCount++;
+            _name = "Player_" + _id;
+        }
+
+        public Player(string name) : this() => _name = name;
+
+        #endregion
+    }
+}
