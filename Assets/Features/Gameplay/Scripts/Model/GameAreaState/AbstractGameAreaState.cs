1e83b995375e7eb8d1f7578fbe7c4d54db0515cf
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2024-12-18 01:28:37 +0700

    Созданы скрипты состояний игрового поля

---
+++ Assets/Features/Gameplay/Scripts/Model/GameAreaState/AbstractGameAreaState.cs
@@ -0,0 +1,37 @@
+﻿namespace TicTacToe3D.Features.Gameplay
+{
+    /// <summary>
+    /// Абстрактное состояние игрового поля
+    /// </summary>
+    public abstract class AbstractGameAreaState
+    {
+        #region Properties
+
+        /// <summary>
+        /// Заблокировано ли игровое поле для взаимодействия
+        /// </summary>
+        public bool IsBlocked => stateType == GameAreaStateType.Blocked;
+
+        /// <summary>
+        /// Тип состояния игрового поля
+        /// </summary>
+        public GameAreaStateType StateType => stateType;
+        protected GameAreaStateType stateType = GameAreaStateType.Default;
+
+        #endregion
+
+        #region Methods
+
+        public AbstractGameAreaState(GameAreaStateType gameAreaStateType)
+            => stateType = gameAreaStateType;
+
+        /// <summary>
+        /// Возможно ли переключиться на заданное состояние
+        /// </summary>
+        /// <param name="nextState"></param>
+        /// <returns></returns>
+        public abstract bool CanSwitchToState(GameAreaStateType nextState);
+
+        #endregion
+    }
+}
