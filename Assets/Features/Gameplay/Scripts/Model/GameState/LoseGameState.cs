0decb7a90f8cb97b08f1c774dec93c0b4651218a
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2024-12-18 01:27:04 +0700

    Созданы скрипты состояний игры

---
+++ Assets/Features/Gameplay/Scripts/Model/GameState/LoseGameState.cs
@@ -0,0 +1,19 @@
+﻿namespace TicTacToe3D.Features.Gameplay
+{
+    /// <summary>
+    /// Состояние игры - проигрыш
+    /// </summary>
+    public class LoseGameState : AbstractGameState
+    {
+        #region Methods
+
+        public LoseGameState(GameStateController gameStateController)
+            : base(gameStateController, GameStateType.Lose)
+        { }
+
+        public override bool CanSwitchToState(GameStateType nextState)
+            => false;
+
+        #endregion
+    }
+}
