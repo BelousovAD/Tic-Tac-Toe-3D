4cebe924e18e6d9271aae160beff4a3fa9393279
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2024-12-18 03:20:22 +0700

    Создан и настроен скрипт отображения слоя позиций спавна шаров

---
+++ Assets/Features/Gameplay/Scripts/View/LayerBallSpawnPositionsView.cs
@@ -0,0 +1,45 @@
+﻿namespace TicTacToe3D.Features.Gameplay
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Zenject;
+
+    /// <summary>
+    /// Отображение слоя позиций спавна шаров
+    /// </summary>
+    public class LayerBallSpawnPositionsView : MonoBehaviour
+    {
+        #region Properties
+
+        [SerializeField]
+        protected TurnController turnController = default;
+        [SerializeField]
+        protected List<GameObject> switchObjects = new();
+
+        protected GameStateController gameStateController = default;
+
+        #endregion
+
+        #region Methods
+
+        [Inject]
+        protected virtual void Construct(GameStateController _gameStateController)
+        {
+            gameStateController = _gameStateController;
+            gameStateController.onStateChanged += SwitchObjects;
+        }
+
+        protected virtual void OnDestroy()
+            => gameStateController.onStateChanged -= SwitchObjects;
+
+        protected virtual void SwitchObjects()
+            => switchObjects.ForEach(
+                x => x.SetActive(
+                !gameStateController.CurrentState.IsBlocked
+                && turnController.CurrentPlayer is not Bot
+                )
+            );
+
+        #endregion
+    }
+}
