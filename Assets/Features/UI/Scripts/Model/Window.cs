5ec47882e5a6ea87ae7752cac066b5fa8e2af20f
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2024-12-19 00:39:47 +0700

    Создан скрипт окна

---
+++ Assets/Features/UI/Scripts/Model/Window.cs
@@ -0,0 +1,49 @@
+﻿namespace TicTacToe3D.Features.UI
+{
+    using System;
+    using TicTacToe3D.Features.Common;
+    using UnityEngine;
+
+    /// <summary>
+    /// Окно
+    /// </summary>
+    public class Window : MonoBehaviour
+    {
+        #region Events
+
+        /// <summary>
+        /// Видимость окна изменилась
+        /// </summary>
+        public event Action onVisibleChanged = delegate { };
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public string Id => id.Value;
+        [SerializeField]
+        protected ID id = default;
+
+        /// <summary>
+        /// Является ли окно видимым
+        /// </summary>
+        public bool IsVisible
+        {
+            get => isVisible;
+            set
+            {
+                if (value != isVisible)
+                {
+                    isVisible = value;
+                    onVisibleChanged();
+                }
+            }
+        }
+        protected bool isVisible = false;
+
+        #endregion
+    }
+}
