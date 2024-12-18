e038482aa3daf37d9da02c9064bf6611f04f2ad1
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2024-12-19 00:39:09 +0700

    Создан скрипт идентификатора

---
+++ Assets/Features/Common/Scripts/ID.cs
@@ -0,0 +1,22 @@
+﻿namespace TicTacToe3D.Features.Common
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
+    [CreateAssetMenu(fileName = nameof(ID), menuName = "TicTacToe3D/Features/Common/New " + nameof(ID))]
+    public sealed class ID : ScriptableObject
+    {
+        #region Properties
+
+        /// <summary>
+        /// Значение
+        /// </summary>
+        public string Value => _value;
+        [SerializeField]
+        private string _value = string.Empty;
+
+        #endregion
+    }
+}
