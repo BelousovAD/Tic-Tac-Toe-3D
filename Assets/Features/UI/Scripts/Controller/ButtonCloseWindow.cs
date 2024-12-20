4691fb70a81e184e8741d85d0d43cb435b512886
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2024-12-21 01:16:52 +0700

    Созданы скрипты кнопок открытия и закрытия окна

---
+++ Assets/Features/UI/Scripts/Controller/ButtonCloseWindow.cs
@@ -0,0 +1,30 @@
+﻿namespace TicTacToe3D.Features.UI
+{
+    using TicTacToe3D.Features.Common;
+    using UnityEngine;
+
+    /// <summary>
+    /// Кнопка закрытия окна
+    /// </summary>
+    public class ButtonCloseWindow : AbstractButton
+    {
+        #region Properties
+
+        protected Window window = default;
+
+        #endregion
+
+        #region Methods
+
+        public override void OnClick()
+            => window.WindowsController.CloseCurrentWindow();
+
+        protected override void Awake()
+        {
+            base.Awake();
+            window = GetComponentInParent<Window>();
+        }
+
+        #endregion
+    }
+}
