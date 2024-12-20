4434b8b2b81721550726995b8099b941ad1ca4d0
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2024-12-21 01:14:47 +0700

    Создан скрипт абстрактной кнопки

---
+++ Assets/Features/Common/Scripts/AbstractButton.cs
@@ -0,0 +1,36 @@
+﻿namespace TicTacToe3D.Features.Common
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    /// <summary>
+    /// Абстрактная кнопка
+    /// </summary>
+    [RequireComponent(typeof(Button))]
+    public abstract class AbstractButton : MonoBehaviour
+    {
+        #region Properties
+
+        private Button _button = default;
+
+        #endregion
+
+        #region Methods
+
+        protected virtual void Awake()
+            => _button = GetComponent<Button>();
+
+        protected virtual void OnEnable()
+            => _button.onClick.AddListener(OnClick);
+
+        protected virtual void OnDisable()
+            => _button.onClick.RemoveListener(OnClick);
+
+        /// <summary>
+        /// Действия при клике на кнопку
+        /// </summary>
+        public abstract void OnClick();
+
+        #endregion
+    }
+}
