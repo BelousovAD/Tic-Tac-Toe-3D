3cf20f886c763fe20f8e1f0d037637bcc5563c11
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2024-12-23 02:12:57 +0700

    Создан скрипт отображения очереди хода

---
+++ Assets/Features/UI/Scripts/View/TurnLabelView.cs
@@ -0,0 +1,49 @@
+﻿namespace TicTacToe3D.Features.UI
+{
+    using TicTacToe3D.Features.Gameplay;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using Zenject;
+
+    /// <summary>
+    /// Отображение очереди хода
+    /// </summary>
+    [RequireComponent(typeof(Text))]
+    public class TurnLabelView : MonoBehaviour
+    {
+        #region Constants
+
+        protected const string TURN_PREFIX = "Turn: ";
+
+        #endregion
+
+        #region Properties
+
+        protected Text textField = default;
+        protected TurnController turnController = default;
+
+        #endregion
+
+        #region Methods
+
+        [Inject]
+        protected virtual void Construct(TurnController _turnController)
+        {
+            turnController = _turnController;
+            turnController.onTurnChanged += UpdateView;
+        }
+
+        protected virtual void Awake()
+            => textField = GetComponent<Text>();
+
+        protected virtual void OnDestroy()
+            => turnController.onTurnChanged -= UpdateView;
+
+        protected virtual void UpdateView()
+        {
+            textField.text = TURN_PREFIX + turnController.CurrentPlayer.Name;
+        }
+
+        #endregion
+    }
+}
